66c794d9ba3f34074f7d0c5835b4e9a7a7c71904
j <j@j.j>
2018-03-06 20:08:00 +0700

    lab4: edit struct of frame

--- Lab4-Protocol-Design/protocol_design.cpp
+++ Lab4-Protocol-Design/protocol_design.cpp
@@ -1,161 +1,189 @@
-#define COM3BASE 0x3E8
-#define COM4BASE 0x2E8
-#define TXDATA COM3BASE
-#define LCR (COM3BASE + 3) /* 0x3F8 line control*/
-#define LSR (COM3BASE + 5) /* 0x3FD line status*/
-#include <conio.h>
-#include <dos.h>
+#define COM3BASE	0x3E8
+#define COM4BASE	0x2E8
+#define TXDATA		COM3BASE
+#define LCR			(COM3BASE + 3)	// 0x3F8 line control
+#define LSR			(COM3BASE + 5)	// 0x3FD line status
+
+/**
+										Type 0 : Data
+		0			1			2		3			4			5				11		12
+	[ START_BIT | START_BIT | TYPE | frame_index | DATA[0] | DATA[1] | ... | DATA[7] | STOPBIT ]
+		0			0			0		1			H			e		...		1		1
+									(send frame 1 : Hell ... \n)
+									( frame size : 11 byte )
+
+
+							Type 1 : ACK
+		0		1				2		3			4
+	[ START_BIT | START_BIT | TYPE | frame_index | STOPBIT ]
+		0			0			6		0			1
+							( ack0)
+					( frame sisze = 4 byte )
+*/
+
+#define FRAME_SIZE				13
+
+#define FRAME_STARTBIT_DATA		0
+#define FRAME_STARTBIT_BEGIN	0
+#define FRAME_STARTBIT_SIZE		2
+#define FRAME_STARTBIT_END		(FRAME_STARTBIT_BEGIN + FRAME_STARTBIT_SIZE -1)
+
+#define	FRAME_TYPE_INDEX		2
+#define FRAME_TYPE_DATA			1
+#define FRAME_TYPE_END			4
+#define FRAME_TYPE_ACK			6
+
+#define FRAME_ID_INDEX			3
+
+#define	FRAME_DATA_BEGIN		4
+#define FRAME_DATA_SIZE			8
+#define FRAME_DATA_END			(FRAME_DATA_BEGIN + FRAME_DATA_SIZE -1)
+
+#define FRAME_STOPBIT_INDEX		12
+#define FRAME_STOPBIT_DATA		1
+
+//#include <conio.h>
+//#include <dos.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-int inchar, outchar;
-char ans;
-
-FILE* fp;
-char *inword, *outword;
-char f_dir[255];
-int status = 0;
-int mode = 1;			/* 0:unread, 1:read */
-int n = 0;
-int isEOF = 0;
-int frame_i = 0;
+typedef struct {
+	char type;
+	char frame_i;
+	char data[FRAME_DATA_SIZE];
+} frame;
 
 void setup_serial(void);
 void send_character(char ch);
 int get_character(void);
-void send_word(char* word);
-char* get_word(void);
+void send_frame(frame* buffer);
+void get_frame(frame* buffer);
+int readFile(FILE* fp, frame* buffer);
+void fill_data(frame* buffer, int i);
+void print_data(frame* buffer);
 
 int main(void) {
+	FILE* fp;
+	char fdir[256];
+
+	frame inword, outword;
+	int mode = 1;						// 0:send in buffer, 1:read to buffer for send
+	char ch;
+
     clrscr();
     setup_serial();
-    printf("Send or Receive ('s' or 'r') : ");
-    ans = getch();
 
-    if (ans == 's') {
+    printf("Send or Receive ('s' or 'r') : ");
+	ch = getch();
+
+    if (ch == 's') {
         printf("Send file : ");
-        gets(f_dir);
-        fp = fopen(f_dir, "r");
+        gets(fdir);
+        fp = fopen(fdir, "r");
         if (!fp) {
-            printf("can't open file dir %s\n", f_dir);
+            printf("can't open file dir %s\n", fdir);
             return -1;
         }
 
-        while (!isEOF) {
+		outword.frame_i = 0;
+        while (1) {
+			// mode = 1, read next word to buffer
             if (mode == 1) {
-                for (i = 0; i < 7; ++i) {
-                    outword[i] = fgetc(fp);
-                    if (outword[i] == EOF) {
-                        for (j = i; j < 7; ++j) {
-                            outword[j] = '\0';
-                        }
-                        isEOF = 1;
-                        break;
-                    }
-                }
-                outword[i] = '\0';
+				if (readFile(fp, &outword) == -1)
+					return 0;
+				outword.type = FRAME_TYPE_DATA;
             }
 
-            send_character(frame_i);
-            send_word(outword);
-
-            printf("\n\nSend frame : %d\n", frame_i);
-            printf("Data       : [");
-            for (i = 0; i < 7; ++i) {
-                if (outword[i] == '\0') {
-                    break;
-                }
-                printf("%c", outword[i]);
-            }
+            printf("\nSend frame : %d\n", outword.frame_i);
+            printf("Data		: [");
+			print_data(&outword);
             printf("]\n");
-            printf("Timeout    : ");
+            printf("Timeout		: ");
 
-            status = get_character();
-            if (status == 'a') {
-                printf("\nReceive ACK%d", get_character());
-                mode = 1;
-                frame_i ^= 1;
-            } else if (status == 't') {
-                printf("t\nRetransmit frame %d", i);
-                mode = 0;
-            } else {  // DEBUG
-                printf("Error! [%d - %c]", status, status);
-            }
+            send_frame(&outword);
+			get_frame(&inword);
+
+			// timeout
+			ch = getch();
+			if (ch == 't') {
+				mode = 0;
+				printf("t\nRetransmit frame %d\n", outword.frame_i);
+				continue;
+			}
+
+			// check ack
+            if (inword.type == FRAME_TYPE_ACK) {
+				mode = 1;
+				outword.frame_i ^= 1;
+				printf("\nReceive ACK%d\n", inword.frame_i);
+			}
         }
-
-        outword[0] = '\0';
-        outword[1] = '\0';
-
-        send_character(frame_i^1);
-        send_word(outword);
+		
+		// dicconnect
+		outword.type = FRAME_TYPE_END;
+		outword.frame_i ^= 1;
+		send_frame(&outword);
 
         fclose(fp);
     }
 
-    if (ans == 'r') {
-        i = 0;
+    if (ch == 'r') {
         printf("Sender Send : ");
-        gets(f_dir);
-        fp = fopen(f_dir, "w+");
+        gets(fdir);
+        fp = fopen(fdir, "w+");
         if (!fp) {
-            printf("can't write file dir %s\n", f_dir);
+            printf("can't write file dir %s\n", fdir);
             return -1;
         }
-        do {
-            frame_i = get_character();
-            inword = get_word();
-            if (inword[0] == '\0') {
-                break;
-            }
-            printf("\n\nReceive frame : %d\n", i);
-            printf("Data          : [%s]\n", inword);
-            printf("Action frame  : ");
-            do {
-                status = getchar();
-            } while (status == '\n');
-            if (status == 'a') {
-                if (frame_i == i) {
-                    printf("Received & Send ACK%d", i + 1);
-                    fputs(inword, fp);
-                    mode = 1;
-                    send_character('a');
-                    send_character(i + 1);
-                } else {
-                    printf("Reject & Send ACK%d", i);
-                    mode = 0;
-                    send_character('a');
-                    send_character(i);
-                }
 
-            } else if (status == 'n') {
-                if (frame_i == i) {
-                    printf("Received & Sleep");
-                    fputs(inword, fp);
-                    mode = 1;
-                } else {
-                    printf("Reject & Sleep");
-                    mode = 0;
-                }
-                send_character('t');
-            } else if (status == 'r') {
-                printf("Reject & Sleep");
-				send_character('t');
-				mode = 0;
-			} else {
-				printf("Error! [%d - %c]", status, status);
+		outword.type = FRAME_TYPE_ACK;
+		outword.frame_i = 0;
+        while (1) {
+			get_frame(&inword);
+			if (inword.type == FRAME_TYPE_END)
+				break;
+
+			printf("\nReceive frame : %d\n", inword.frame_i);
+			printf("Data          : [");
+			print_data(&inword);
+			printf("]\n");
+			printf("Action frame  : ");
+
+			ch = getchar();
+			switch (ch) {
+				case 'a' :
+					if (outword.frame_i == inword.frame_i) {
+						fputs(inword.data, fp);
+						outword.frame_i ^= 1;
+		                printf("Received & Send ACK%d\n", outword.frame_i);
+					} else {
+						printf("Reject & Send ACK%d\n", outword.frame_i);
+					}
+				break;
+
+				case 'n' :
+					if (outword.frame_i == inword.frame_i) {
+						outword.frame_i ^= 1;
+						fputs(inword.data, fp);
+						printf("Received & Sleep\n");
+					} else {
+						printf("Reject & Sleep\n");
+					}
+				break;
+
+				case 'r' :
+					printf("Reject & Sleep\n");
+				break;
 			}
-
-			if (mode == 1) {
-				i ^= 1;
-			}
-
-		} while (1);
+		}
 
 		fclose(fp);
 	}
 
 	return 0;
 }
+
 void setup_serial(void) {
 	outportb(LCR, 0x80);
 	/*set up bit 7 to a 1 to set Register address bit*/
@@ -170,9 +198,9 @@ void setup_serial(void) {
 	/* 1  - parity on, 0 – 1 stop bit */
 	/* 10 – 7 data bits */
 }
+
 void send_character(char ch) {
 	char status;
-
 	do {
 		status = inportb(LSR) & 0x40;
 	} while (status != 0x40);
@@ -189,19 +217,74 @@ int get_character(void) {
 	return ((int)inportb(TXDATA));
 }
 
-void send_word(char* word) {
-	int i = 0;
-	for (i = 0; i < 7; ++i) {
-		send_character(word[i]);
+void send_frame(frame* buffer) {
+	send_character(FRAME_STARTBIT_DATA);
+	send_character(FRAME_STARTBIT_DATA);
+	send_character(buffer->type);
+	send_character(buffer->frame_i);
+	int i;
+	for (i = 0; i < FRAME_DATA_SIZE; ++i) {
+		send_character(buffer->data[0]);
 	}
-	send_character('\0');
+	send_character(FRAME_STOPBIT_DATA);
 }
 
-char* get_word(void) {
+void get_frame(frame* buffer) {
 	int i = 0;
-	char* word;
+	char tmp;
+
+	// detect start bit
 	do {
-		word[i++] = get_character();
-	} while (word[i - 1] != '\0');
-	return word;
+		tmp = get_character();
+		if (tmp == FRAME_STARTBIT_DATA) {
+			++i;
+		} else {
+			i = 0;
+			fprintf(stderr, "startbit detect error");
+		}
+	} while(i == FRAME_STARTBIT_SIZE);
+
+	// read to buffer
+	buffer->type = get_character();
+	buffer->frame_i = get_character();
+	for (i = FRAME_DATA_BEGIN; i <= FRAME_DATA_END; ++i) {
+		buffer->data[i] = get_character();
+	}
+
+	// detect stopbit
+	tmp = get_character();
+	if (tmp != FRAME_STOPBIT_DATA) {
+		fprintf(stderr, "stopbit detect error");
+	}
+}
+
+int readFile(FILE* fp, frame* buffer) {
+	int i;
+	for (i = 0; i < FRAME_DATA_SIZE; ++i) {
+		buffer->data[i] = fgetc(fp);
+		// is End of File
+		if (buffer->data[i] == EOF) {
+			fill_data(buffer, i);
+			return -1;
+		}
+	}
+	//fill_data(buffer, i);
+	printf(">>readFile: {i:%d}",i);
+	return 0;
+}
+
+void fill_data(frame* buffer, int i) {
+	for(; i <= FRAME_DATA_SIZE; ++i) {
+		buffer->data[i] = '\0';
+	}
+}
+
+void print_data(frame* buffer) {
+	int i;
+	for (i = 0; i < FRAME_DATA_SIZE; ++i) {
+		if (buffer->data[i] == '\0') {
+			break;
+		}
+		printf("%c", buffer->data[i]);
+	}
 }
