861619900b54fc1e6e6590d0156a00868e2f6f47
j <j@j.j>
2018-05-11 22:58:33 +0700

    addComment Lab6-Signal-SineWaveArduino

--- Lab6-Signal/SineWaveArduino/SineWaveArduino.ino
+++ Lab6-Signal/SineWaveArduino/SineWaveArduino.ino
@@ -1,36 +1,36 @@
 #include <Wire.h>
 #include <Adafruit_MCP4725.h>
 
-#define defaultFreq 1700
-#define freq0 500
-#define N_BIT 4
+#define defaultFreq 1700        /* DAC Speed */
+#define freq0 500               /* frequency of carrier size wave (Hz) */
+#define N 4                     /* number of angle */
 
 Adafruit_MCP4725 dac;
-int delay0;
+float zeta[N_BIT];              /* N=4, zeta : 0, 90, 180, 270 */
+float S[N_BIT];                 /* Sinewave of zeta */
+uint16_t S_DAC[N_BIT];          /* DutyCycle PWM of sizewave */
+int delay0;                     /* delay for period of sizewave */
 char str[80];
-float zeta[N_BIT];
-float S[N_BIT];
-uint16_t S_DAC[N_BIT];
 
 void setup() {
-
   Serial.begin(115200);
   dac.begin(0x64);
-  delay0 = (1000000 / freq0 - 1000000 / defaultFreq) / N_BIT;
+  delay0 = (1000000 / freq0 - 1000000 / defaultFreq) / N;
   sprintf(str, "delay0:", delay0);
   Serial.println(str);
 
-  for (int i = 0; i < N_BIT; ++i) {
-    zeta[i] = i * (360 / N_BIT);
-    S[i] = sin(zeta[i] * (M_PI / 180));
-    S_DAC[i] = ((S[i] + 1) / 2) * 255;
+  for (unsigned i=0; i<N; ++i) {
+    zeta[i] = i * (360 / N);                /* geb zeta (deg) */
+    S[i] = sin(zeta[i] * (M_PI / 180));     /* covent zeta(deg) to sin(rad) */
+    /* MCP4725 DAC pwm (0-4095) */
+    S_DAC[i] = ((S[i] + 1) / 2) * 4095;      /* gen DutyCycle PWM from sine */ 
     sprintf(str, "zeta:%6s, sine:%5s, S_DAC:%3d", String(zeta[i]).c_str(), String(S[i]).c_str(), S_DAC[i]);
     Serial.println(str);
   }
 }
 
 void loop() {
-  for (int i = 0; i < N_BIT; ++i) {
+  for (unsigned i=0; i<N; ++i) {
     dac.setVoltage(S_DAC[i], false);
     delayMicroseconds(delay0);
   }
