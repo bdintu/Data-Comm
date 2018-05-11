7f0adb4052b97df42cc3b6ee086a48ba0add2e56
j <j@j.j>
2018-05-11 22:46:52 +0700

    addComment Lab6-Signal-PulseWaveArduino

--- Lab6-Signal/PulseWaveArduino/PulseWaveArduino.ino
+++ Lab6-Signal/PulseWaveArduino/PulseWaveArduino.ino
@@ -1,26 +1,27 @@
 #define PWM_PIN 6
-#define N_BIT 4
+#define N 4
 
+float zeta[N];              /* N=4, zeta : 0, 90, 180, 270 */
+float S[N];                 /* Sinewave of zeta*/
+uint16_t pwmDuty[N];        /* DutyCycle PWM of sinewave */
 char str[80];
-float zeta[N_BIT];
-float S[N_BIT];
-uint16_t pwmDuty[N_BIT];
 
 void setup() {
   Serial.begin(115200);
   pinMode(PWM_PIN, OUTPUT);
 
-  for (int i = 0; i < N_BIT; ++i) {
-    zeta[i] = i * (360 / N_BIT);
-    S[i] = sin(zeta[i] * (M_PI / 180));
-    pwmDuty[i] = ((S[i] + 1) / 2) * 255;
+  for (unsigned i=0; i<N; ++i) {
+    zeta[i] = i * (360 / N);                /* gen zeta (deg) */
+    S[i] = sin(zeta[i] * (M_PI / 180));     /* covent zeta(deg) to sine (rad) */
+    /* arduino uno pwm (0-255) */
+    pwmDuty[i] = ((S[i] + 1) / 2) * 255;    /* gen DutyCycle PWM from sine */
     sprintf(str, "zeta:%6s, sine:%5s, pwmDuty:%3d", String(zeta[i]).c_str(), String(S[i]).c_str(), pwmDuty[i]);
     Serial.println(str);
   }
 }
 
 void loop() {
-  for (int i = 0; i < N_BIT; ++i) {
+  for (unsigned i=0; i<N; ++i) {
     analogWrite(PWM_PIN, pwmDuty[i]);
     delayMicroseconds(400);
   }
