5dac89f871b1ac14ce8de1eee8a43776ee258582
j <j@j.j>
2018-05-11 22:34:00 +0700

    add Lab9-Digital-Modu ASK-TX

--- Lab9-Digital-Modulation/ASK-TX/ASK-TX.ino
+++ Lab9-Digital-Modulation/ASK-TX/ASK-TX.ino
@@ -1,14 +1,15 @@
 #include <Wire.h>
 #include <Adafruit_MCP4725.h>
 
-#define defaultFreq 1700
-#define freq0 500
+#define defaultFreq 1700            /* DAC Speed */
+#define freq0 1000                  /* frequency of carrier sine wave (Hz) */
+#define N_DATA 1                    /* Number of Data */
 
 Adafruit_MCP4725 dac;
-const float A[4] = {_,_,_,_};
-const uint16_t S_DAC[4] = {_,_,_,_};
+const float A[4] = {1 ,2 , 3, 4};   /* ASK Amplitude (0, 5] (V) */
+const uint16_t S_DAC[4] = {2047 ,4095 ,2047 , 0};   /* Amplitude (12bit) of sinewave at 0, 90, 180, 270 */
 int delay0;
-char inData[20];
+char inData[N];
 
 void setup() {
     Serial.begin(115200);
@@ -18,22 +19,27 @@ void setup() {
 }
 
 void loop() {
-   if (Serial.available() > 0) {
-    for (unsigned i=0; i<20; ++i) {
+   if (Serial.available()) {
+    for (unsigned i=0; i<N; ++i) {
         inData[i] = Serial.read();
     }
 
-    for (unsigned i=0; i<20; ++i) {
+    /* use a cycle loop i for sebd data 8 bits */
+    for (unsigned i=0; i<N; ++i) {
+        /* use a cycle loop k for 1 ASK signal element (2 bit) LSB-> MSB */
         for (unsigned k=7; k>=0; k-=2) {
-            int tmp = inData[i] & 3;
-
+            int tmp = inData[i] & 0x03;                 /* 00 01 10 11 */
+            /* use a cycle loop sl to send 5 cycle/baud */
             for (unsigned sl=0; sl<5; ++sl) {
+                /* use a cycle loop s to send 4 sample/cycle */
                 for (unsigned s=0; s<4; ++s) {
-                    dac.setVoltage(S_DAC[s], false);
+                    float outData = A[tmp]*S_DAC[s] /4;
+                    Serial.println(outData);
+                    dac.setVoltage(outData, false);     /* (mV) */
                     delayMicroseconds(dalay0);
                 }
             }
-            inData[i] >>= 2;
+            inData[i] >>= 0x02;
         }
     }
 
