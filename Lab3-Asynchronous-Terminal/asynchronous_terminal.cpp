fc82503eb1a6c9b6f7a6b1ae66b517b7247d4c74
j <j@j.j>
2018-05-12 02:00:53 +0700

    add Lab3-Async-Terminal async_terminal

--- Lab3-Asynchronous-Terminal/asynchronous_terminal.cpp
+++ Lab3-Asynchronous-Terminal/asynchronous_terminal.cpp
@@ -1,100 +1,62 @@
 #define COM3BASE 0x3E8
 #define COM4BASE 0x2E8
 #define TXDATA COM3BASE
-#define LCR (COM3BASE+3) /* 0x3F8 line control*/
-#define LSR (COM3BASE+5) /* 0x3FD line status*/
+#define LCR (COM3BASE+3)	/* 0x3F8 line control*/
+#define LSR (COM3BASE+5)	/* 0x3FD line status*/
+
 #include <conio.h>
 #include <dos.h>
 #include <stdio.h>
 #include <string.h>
 
 void setup_serial(void);
-void send_character(char ch);
+void send_character(char);
 int get_character(void);
+void send_string(void);
+void get_string(void);
+
+int inchar, outchar;	/* serial buffer */
+char mode;				/* 0:Sender, 1:Receive */
+char str[128];					/* keyboad buffer */
+int i=0;
 
 int main(void) {
-	int inchar, outchar;
-	char send[128];
-	int size;
-	char ans;
-	int i;
-
 	setup_serial();
-	printf("Send or Receive : ");
-	ans = getch();
+	printf("Send(s) or Receive(r) : ");
+	mode = getch();
 
-	if (ans == 's') {
+	if (mode == 's') {		/* Mode Sender */
 		while(1) {
-			printf("Enter char to be transmitted (Ctrl-D to end)");
-			printf("PC1 : ");
-			gets(send);
-			for(i=0;i<strlen(send);i++) {
-				send_character(send[i]);
-			}
-			send_character('\0');
-
-			puts("PC2 : ");
-			while(1) {
-				inchar = get_character();
-				if (inchar == 17) {
-					send_character(17);
-					return 0;
-				}
-				printf("%c",inchar);
-
-				if(inchar == '\0') {
-					printf("\n");
-					break;
-				}
-			}
+			send_string();
+			get_string();
 		}
 	}
 
-	if (ans == 'r') {
+	if (mode == 'r') {		/* Mode Receive */
 		while(1) {
-			printf("PC2 : ");
-			while(1){
-				inchar = get_character();
-				if (inchar == 17) {
-					send_character(17);
-					return 0;
-				}
-				printf("%c",inchar);
-
-				if (inchar == '\0') {
-					printf("\n");
-					break;
-				}
-			}
-
-			printf("Enter char to be transmitted (Ctrl-D to end)");
-			printf("PC1 : ");
-			gets(send);
-			for(i=0;i<strlen(send);i++) {
-				send_character(send[i]);
-			}
-			send_character('\0');
+			get_string();
+			send_string();
 		}
 	}
 
 	return 0;
 }
+
 void setup_serial(void) {
 	outportb(LCR, 0x80);
 	/*set up bit 7 to a 1 to set Register address bit*/
 	outportb(TXDATA,0x0C);
 	outportb(TXDATA+1,0X00);
 	outportb(LCR, 0xA);
-	/*load TxRegister with 12, crystal frequency is 1.8432 MHz*/  outportb(LCR, 0x0A);
+	/*load TxRegister with 12, crystal frequency is 1.8432 MHz*/  		outportb(LCR, 0x0A);
 	/* Bit pattern loads is 00001010b, from MSB to LSB these are: */
 	/* 0  - access TD/RD buffer, 0 - normal output */
 	/* 0  - no stick bit, 0 - even parity */
 	/* 1  - parity on, 0 – 1 stop bit */
 	/* 10 – 7 data bits */
 }
-void send_character (char ch) {
+void send_character(char ch) {
 	char status;
-
 	do{
 		status = inportb(LSR) & 0x40;
 	} while ( status!=0x40);
@@ -108,4 +70,31 @@ int get_character(void) {
 	}while (status!=0x01);
 	/*Repeat until bit 1 in LSR is set */
 	return( (int) inportb(TXDATA));
+}
+
+void send_string(void) {
+	printf("Enter char to be transmitted (Ctrl-D to end)");
+	printf("PC1 : ");
+	gets(str);						/* get string until enter */
+	for (i=0; i<strlen(str); i++) {
+		send_character(str[i]);		/* send string to serial */
+	}
+	send_character('\0');			/* send null */
+}
+
+void get_string(void) {
+	puts("PC2 : ");
+	while(1) {						/* read char until 17 or null */
+		inchar = get_character();
+		if (inchar == 17) {			/* Ctrl-D */
+			send_character(17);		/* ack for end connection */
+			return 0;
+		}
+		printf("%c", inchar);
+
+		if(inchar == '\0') {		/* found null */
+			printf("\n");
+			break;
+		}
+	}
 }
